2ea87bab4f1b630df4dfd6ae141e79e54f415197
Yuri Matheus <yurimatheusb@hotmail.com>
2019-02-23 22:37:49 -0300

    adds spring boot project

---
+++ src/main/java/br/com/fiap/maquininhas/MaquininhasApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.maquininhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaquininhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaquininhasApplication.class, args);
+	}
+
+}
