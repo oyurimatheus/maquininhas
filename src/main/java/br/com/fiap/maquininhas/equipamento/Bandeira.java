50f328e05e2fac025dcf094f6ae199683892914f
Yuri Matheus <yurimatheusb@hotmail.com>
2019-02-24 02:51:10 -0300

    cria bandeira dos cartoes

---
+++ src/main/java/br/com/fiap/maquininhas/equipamento/Bandeira.java
@@ -0,0 +1,23 @@
+package br.com.fiap.maquininhas.equipamento;
+
+public enum Bandeira {
+
+    MASTERCARD,
+    VISA,
+    ELO,
+    HIPERCARD,
+    HIPER,
+    CANAL,
+    DINERS,
+    AMERICAN_EXPRESS,
+
+    MAESTRO,
+    VISA_ELECTRON,
+    BANRI_COMPRAS,
+    CABAL_DEBITO,
+
+    ALELO,
+    SODEXO,
+    TICKET,
+    VR
+}
