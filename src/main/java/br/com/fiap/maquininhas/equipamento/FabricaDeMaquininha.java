c6d7f8a70c52110c2f62e141dc8d3f3771fd9ed2
Yuri Matheus <yurimatheusb@hotmail.com>
2019-02-24 03:57:51 -0300

    tira construtor privado para injetar repositorios

--- src/main/java/br/com/fiap/maquininhas/equipamento/FabricaDeMaquininha.java
+++ src/main/java/br/com/fiap/maquininhas/equipamento/FabricaDeMaquininha.java
@@ -1,61 +1,34 @@
 package br.com.fiap.maquininhas.equipamento;
 
-import java.math.BigDecimal;
+import br.com.fiap.maquininhas.equipamento.repositorios.CartaoRepositorio;
+import br.com.fiap.maquininhas.equipamento.repositorios.VoucherRepositorio;
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import static br.com.fiap.maquininhas.equipamento.Bandeira.*;
-import static br.com.fiap.maquininhas.equipamento.Comprovante.EMAIL;
-import static br.com.fiap.maquininhas.equipamento.Comprovante.IMPRESSO;
-import static br.com.fiap.maquininhas.equipamento.Comprovante.SMS;
-import static br.com.fiap.maquininhas.equipamento.Conexao.BLUETOOTH;
-import static br.com.fiap.maquininhas.equipamento.Conexao.CHIP_4G;
-import static br.com.fiap.maquininhas.equipamento.Conexao.WIFI;
-import static br.com.fiap.maquininhas.equipamento.TipoDoCartao.CREDITO;
-import static br.com.fiap.maquininhas.equipamento.TipoDoCartao.DEBITO;
+import static br.com.fiap.maquininhas.equipamento.Comprovante.*;
+import static br.com.fiap.maquininhas.equipamento.Conexao.*;
 import static br.com.fiap.maquininhas.equipamento.TipoDoTeclado.TOUCHSCREEN;
 import static br.com.fiap.maquininhas.equipamento.Visor.COLORIDO_TOUCHSCREEN;
 import static br.com.fiap.maquininhas.equipamento.Voltagem.BIVOLT;
 
+@Component
 public class FabricaDeMaquininha {
 
-    private static TaxaDeCompra taxaDeCompraPadraoCredito = new TaxaDeCompra(new BigDecimal("3.19"),
-            new BigDecimal("3.79"),
-            "Vendas parceladas, acréscimo de 2,99% ao mês");
-    private static TaxaDeCompra taxaDeCompraPadraoDebito = new TaxaDeCompra(new BigDecimal("1.99"));
 
+    private CartaoRepositorio cartaoRepositorio;
+    private VoucherRepositorio voucherRepositorio;
 
-    private FabricaDeMaquininha() {}
+    public FabricaDeMaquininha(CartaoRepositorio cartaoRepositorio, VoucherRepositorio voucherRepositorio) {
+        this.cartaoRepositorio = cartaoRepositorio;
+        this.voucherRepositorio = voucherRepositorio;
+    }
 
-    public static Maquininha criaModerninhaSmart() {
+    public  Maquininha criaModerninhaSmart() {
+        List<Cartao> cartoes = cartaoRepositorio.findAll();
 
-       final TaxaDeCompra taxaDeCompraCartoesCreditoInternacionais = new TaxaDeCompra(new BigDecimal("3.59"),
-                new BigDecimal("4.19"),
-                "Vendas parceladas, acréscimo de 2,99% ao mês");
-
-        List<Cartao> cartoes = new ArrayList<Cartao>() {{
-            add(new Cartao(CREDITO, MASTERCARD, taxaDeCompraPadraoCredito));
-            add(new Cartao(CREDITO, VISA, taxaDeCompraPadraoCredito));
-            add(new Cartao(CREDITO, ELO, taxaDeCompraPadraoCredito));
-            add(new Cartao(CREDITO, HIPERCARD, taxaDeCompraPadraoCredito));
-            add(new Cartao(CREDITO, HIPER, taxaDeCompraPadraoCredito));
-            add(new Cartao(CREDITO, CANAL, taxaDeCompraPadraoCredito));
-            add(new Cartao(CREDITO, AMERICAN_EXPRESS, taxaDeCompraCartoesCreditoInternacionais));
-            add(new Cartao(CREDITO, DINERS, taxaDeCompraCartoesCreditoInternacionais));
-
-            add(new Cartao(DEBITO, MAESTRO, taxaDeCompraPadraoDebito));
-            add(new Cartao(DEBITO, VISA_ELECTRON, taxaDeCompraPadraoDebito));
-            add(new Cartao(DEBITO, ELO, taxaDeCompraPadraoDebito));
-            add(new Cartao(DEBITO, BANRI_COMPRAS, taxaDeCompraPadraoDebito));
-            add(new Cartao(DEBITO, CABAL_DEBITO, taxaDeCompraPadraoDebito));
-        }};
-
-        List<Voucher> vouchers = new ArrayList<Voucher>() {{
-            add(new Voucher(ALELO, "A aceitação de voucher depende de contrato com a bandeira"));
-            add(new Voucher(SODEXO, "A aceitação de voucher depende de contrato com a bandeira"));
-            add(new Voucher(TICKET, "A aceitação de voucher depende de contrato com a bandeira"));
-            add(new Voucher(VR, "A aceitação de voucher depende de contrato com a bandeira"));
-        }};
+        List<Voucher> vouchers = voucherRepositorio.findAll();
 
         List<Conexao> conexoes = new ArrayList<Conexao>(){{
             add(BLUETOOTH);
